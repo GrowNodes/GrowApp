b28d2a530750db26d75d15f30a20b75e7a99df44
Kevin <keyeh@calpoly.edu>
2017-01-22 14:04:14 -0800

    remove water pump from manually controlled devices

--- src/containers/ManuallyControlledDevices.js
+++ src/containers/ManuallyControlledDevices.js
@@ -1,36 +1,31 @@
-import React, {Component, PropTypes} from 'react';
-import { connect } from 'react-redux';
-import {mqttSend} from '../actions/mqtt'
-import {
-  Switch
-} from 'react-onsenui';
+import React, {Component, PropTypes} from "react";
 
+import {connect} from "react-redux";
+
+import {mqttSend} from "../actions/mqtt";
+
+import {Switch} from "react-onsenui";
 
 class ManuallyControlledDevices extends Component {
-    
-    handleAirPumpChange(e) {
-        const msgToSend = e.target.checked ? "true" : "false"
-        this.props.mqttSend(`${this.props.node.serial}/air_pump/on/set`, msgToSend)
-    }
+  handleAirPumpChange(e) {
+    const msgToSend = e.target.checked ? "true" : "false";
 
-    handleWaterPumpChange(e) {
-        const msgToSend = e.target.checked ? "true" : "false"
-        this.props.mqttSend(`${this.props.node.serial}/water_pump/on/set`, msgToSend)
-    }
+    this.props.mqttSend(`${this.props.node.serial}/air_pump/on/set`, msgToSend);
+  }
 
-    render () {
-        return (
-            <div>
-            Air Pump <Switch checked={this.props.node["air_pump/on"]} onChange={this.handleAirPumpChange.bind(this)} />
-            <br/>
-            Water Pump <Switch checked={this.props.node["water_pump/on"]} onChange={this.handleWaterPumpChange.bind(this)} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        Air Pump<Switch checked={this.props.node["air_pump/on"]} onChange={
+          this.handleAirPumpChange.bind(this)
+        } />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   node: state.user_nodes[state.selectedUserNode]
 });
 
-export default connect(mapStateToProps, {mqttSend})(ManuallyControlledDevices);
+export default connect(mapStateToProps, { mqttSend })(ManuallyControlledDevices)
